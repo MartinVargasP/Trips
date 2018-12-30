b7bc3b8ec1d918a4a2080258cef3da1ce7f9cd1b
Brayan Mamani <brayanmamani.developer@gmail.com>
2018-12-30 13:30:09 -0400

    Description Place

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'description_place.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  String descriptionDummy = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,10 +28,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Hola Mundo Feliz'),
         ),
-        body: Center(
-          child: Text('Hola Mundo Platzi'),
+        body: new Stack(
+          children: <Widget>[
+            DescriptionPLace('Bahamas',4,descriptionDummy),
+          ],
+        )
         ),
-      )
     );
   }
 }
