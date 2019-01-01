431297c916986b6a473d12dba24a0f0876030b10
Brayan Mamani <brayanmamani.developer@gmail.com>
2019-01-01 19:34:19 -0400

    Trips 1.2

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -10,9 +10,20 @@ class ProfileTrips extends StatelessWidget{
     return Stack(
       children: <Widget>[
         GradientProfile('Profile'),
+        Container(
+          margin: EdgeInsets.only(
+            top: 55.0,
+            left: 370.0
+          ),
+          child: Icon(
+            Icons.settings,
+            color: Colors.white70,
+            size: 18.0,
+          ),
+        ),
         ProfilePeople('assets/img/Adrian.png'),
         OptionProfile(),
-        CardImageProfileList()
+        CardImageProfileList(),
       ],
     );
   }
